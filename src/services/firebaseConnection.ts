db47f528d96cd4d2e94ad0f327b26aee6f8cc99a
mikek182 <marcosvinicius@geekwork.com.br>
2025-09-20 18:50:34 -0300

    next auth

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -1,20 +1,21 @@
-
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD55XduV2H9c38KlO4SJDX9cKYtTG1me44",
-    authDomain: "to-do-list-next-fe4b7.firebaseapp.com",
-    projectId: "to-do-list-next-fe4b7",
-    storageBucket: "to-do-list-next-fe4b7.firebasestorage.app",
-    messagingSenderId: "838152249179",
-    appId: "1:838152249179:web:cca5d4154d5ac7cfc1c47e",
-    measurementId: "G-3WKMR61DMJ"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
+// Inicializa o Firebase apenas se não houver aplicativos inicializados
+const firebaseApp = getApps().length === 0 
+    ? initializeApp(firebaseConfig) 
+    : getApp();
 
-const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
-// const analytics = getAnalytics(firebaseApp);
+
 export { db };
